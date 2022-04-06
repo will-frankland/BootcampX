dde2b1f47d0ffbc1728fe5bed91963d7315acd03
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-04-06 16:05:44 -0400

    paramatized students and teachers js files

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,8 @@
 const req = require('express/lib/request');
 const { Pool } = require('pg');
-process.argv.slice(2);
+const cohortName = process.argv[2]
+const limit = process.argv[3] || 5;
+const values = [cohortName, limit]
 
 const pool = new Pool({
   user: 'vagrant',
@@ -11,15 +13,14 @@ const pool = new Pool({
 
 
 pool.query(`
-SELECT students.id as student_id, students.name as student_name, cohorts.name as cohort_name
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts on cohorts.id = students.cohort_id
-LIMIT 5;
-`)
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(user)
-    console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-})
-.catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,7 @@
 const req = require('express/lib/request');
 const { Pool } = require('pg');
 const cohortName = process.argv[2]
+const values = [cohortName]
 
 const pool = new Pool({
   user: 'vagrant',
@@ -9,7 +10,6 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-console.log('cohortName: ', cohortName);
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -17,12 +17,12 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
-    console.log(row.teacher)
+    console.log(row)
   })
 })
 .catch(err => console.error('query error', err.stack));
